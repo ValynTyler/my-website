a81431ecf6625b55bee0ae5284618666af97bbbd
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-03-27 00:26:33 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{fs::File, io::Read};
-
 #[macro_use] extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "
-    
+    - itch: https://valyn-tyler.itch.io
+    - twitter: https://twitter.com/ValynTyler
+    - discord: `valyntyler`
     "
 }
 

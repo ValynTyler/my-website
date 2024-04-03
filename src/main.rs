58aa795bec8977f37c2377898a011e4f8107a9e0
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-04-03 15:55:30 +0300

    Page contains links

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use rocket::fs::NamedFile;
 
 #[get("/")]
 async fn index() -> Option<NamedFile> {
-    NamedFile::open("index.html").await.ok()
+    NamedFile::open("web/index.html").await.ok()
+}
+
+#[get("/styles.css")]
+async fn css() -> Option<NamedFile> {
+    NamedFile::open("web/styles.css").await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, css])
 }
